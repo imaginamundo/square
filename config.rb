7b2623ca80a22c12c1f80df5434310456b3426d1
Diogo Ferreira Fernandes <dio@Mac-mini-de-Diogo-2.local>
2015-04-11 20:17:18 -0300

    Teste novo repositorio

---
+++ config.rb
@@ -0,0 +1,21 @@
+# Require any additional compass plugins here.
+
+# Folder settings
+css_dir = "css"          #where the CSS will saved
+sass_dir = "sass"         #where our SASS files are
+images_dir = "img"       #the folder with your images
+fonts_dir = "font"       #the folder with your fonts
+javascripts_dir = "js"   #the folder with your js
+
+# Relative paths to assets via compass helper functions.
+relative_assets = true
+
+# You can select your preferred output style here.
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
+
+# Obviously
+preferred_syntax = :scss
